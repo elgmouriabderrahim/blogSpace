cbd53710039a023b7b173fe20486c56ca71ccacc
elgmouri abderrahim <ea.officialbox@gmail.com>
2026-01-06 10:00:46 +0000

    add register and login pages

--- public/index.php
+++ public/index.php
@@ -6,4 +6,7 @@ session_start();
 
 $router = new Router();
 $router->get('/', "HomeController@index");
+$router->get('/register', "AuthController@register");
+$router->get('/login', "AuthController@login");
+$router->get('/logout', "AuthController@logout");
 $router->dispatch();
